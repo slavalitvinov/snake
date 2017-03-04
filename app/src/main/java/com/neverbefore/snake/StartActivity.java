e27af162063d5d7d7728b032e7c7df0b6fa8d9b7
slavalitvinov <slava.litvinov@gmail.com>
2017-03-04 10:17:07 -0800

    First commit

---
+++ app/src/main/java/com/neverbefore/snake/StartActivity.java
@@ -0,0 +1,77 @@
+package com.neverbefore.snake;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        findViewById(R.id.buttonExit).setOnClickListener( this );
+        findViewById(R.id.buttonNewGame).setOnClickListener( this );
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if ( view.getId() == R.id.buttonExit ) {
+            this.finish();
+        }
+        else if ( view.getId() == R.id.buttonNewGame ) {
+            Intent i = new Intent(this, FullscreenActivity.class);
+            //i.setAction(Intent.ACTION_MAIN);
+            //i.addCategory(Intent.CATEGORY_LAUNCHER);
+            startActivity(i);
+        }
+    }
+}
